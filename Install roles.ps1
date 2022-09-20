f9c39e445d2771d3705c5f7ad48c9a46927daf87
VerstraetenYitro <yitro.verstraeten@student.howest.be>
2022-09-20 21:24:05 +0200

    test

--- Install roles.ps1
+++ Install roles.ps1
@@ -1,18 +1,23 @@
 ﻿
 $computerName = "win11-DC1"
-$installingRolesNames = "DHCP","DNS","AD-Domain-Services" #to get list of all roles run "Get-WindowsFeature"
+$installingRolesNames = "AD-Domain-Services","DHCP","DNS" #to get list of all roles run "Get-WindowsFeature"
 
 
+try
+{
 #control if roles are already installed, if not install the role.
 
-for($i = 0;$i -le $installingRolesNames.Length;$i++)
-{
-    $alreadyInstalled = Get-WindowsFeature -Name $installingRolesNames[$i - 1] | % Installed
-    if(-not $alreadyInstalled)
+    for($i = 0;$i -le $installingRolesNames.Length;$i++)
     {
-        Install-WindowsFeature -Name $installingRolesNames[$i - 1] -computerName $computerName
+        $alreadyInstalled = Get-WindowsFeature -Name $installingRolesNames[$i - 1] | % Installed
+        if(-not $alreadyInstalled)
+        {
+            Install-WindowsFeature -Name $installingRolesNames[$i - 1] -computerName $computerName -IncludeManagementTools -Restart
+        } 
     }
-       
 }
+catch
+{}
+
  
  
