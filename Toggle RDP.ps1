c1aedf2b7037b29472a5d70775b6c954685411c3
VerstraetenYitro <yitro.verstraeten@student.howest.be>
2022-09-21 09:07:00 +0200

    new scripts

---
+++ Toggle RDP.ps1
@@ -0,0 +1,9 @@
+﻿$RDPStatus=Get-
+
+Enable Remote Desktop: Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+
+Disable Remote Desktop: Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 1
+
+Enable Remote Desktop From Firewall: Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+Disable Remote Desktop From Firewall: Disable-NetFirewallRule -DisplayGroup "Remote Desktop"
---
+++ set_hostname.ps1
@@ -0,0 +1,7 @@
+﻿#
+#set_hostname
+#
+$current_hostname=hostname
+$new_hostname="win11-DC1"
+
+Rename-Computer -ComputerName $current_hostname -NewName $new_hostname -Restart -Confirm:$false
