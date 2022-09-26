dcd4b30d034a7c4fd168c9a41d7baf03086694ae
unknown <Administrator@win11-DC1.intranet.mijnschool.be>
2022-09-26 12:14:56 +0200

    test

--- Powershell/Opdracht1/promote_dc.ps1
+++ Powershell/Opdracht1/promote_dc.ps1
@@ -1,36 +1,27 @@
 ﻿$computerName = "win11-DC1"
 $installingRolesNames = "AD-Domain-Services","DHCP","DNS" #to get list of all roles run "Get-WindowsFeature"
+$credential="administrator"
+$domainCredential="$env:USERDOMAIN\$credential"
 
 
-try
-{
+
 #control if roles are already installed, if not install the role.
 
-    for($i = 0;$i -le $installingRolesNames.Length;$i++)
-    {
-        $alreadyInstalled = Get-WindowsFeature -Name $installingRolesNames[$i - 1] | % Installed
-        if(-not $alreadyInstalled)
-        {
-            Install-WindowsFeature -Name $installingRolesNames[$i - 1] -computerName $computerName -IncludeManagementTools -Restart
-        } 
-    }
-}
-catch
-{}
+
+$alreadyInstalled = Get-WindowsFeature -Name $installingRolesNames[$i - 1] | % Installed
+if(-not $alreadyInstalled)
+{
+    Install-WindowsFeature -Name $installingRolesNames[$i - 1] -computerName $computerName -IncludeManagementTools -Restart
+} 
+
+
 
 
 # Create New Forest, add Domain Controller
-$domainname = “nwtraders.msft”
-$netbiosName = “NWTRADERS”
-Import-Module ADDSDeployment
-Install-ADDSForest -CreateDnsDelegation:$false `
--DatabasePath “C:\Windows\NTDS” `
--DomainMode “Win2012” `
--DomainName $domainname `
--DomainNetbiosName $netbiosName `
--ForestMode “Win2012” `
--InstallDns:$true `
--LogPath “C:\Windows\NTDS” `
--NoRebootOnCompletion:$false `
--SysvolPath “C:\Windows\SYSVOL” `
--Force:$true
+$domainname = “intranet.mijnschool.be”
+$netbiosName = “mijnschool”
+Install-ADDSForest -DomainName "$domainname" -InstallDNS
+
+
+#add DC
+Install-ADDSDomainController -InstallDns -Credential $credential -DomainName $domainname
