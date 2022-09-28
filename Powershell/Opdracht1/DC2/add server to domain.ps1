506380d92d57b76eaaf391bfc7f2a757818c2fdd
unknown <administrator@INTRANET.MIJNSCHOOL.BE>
2022-09-28 12:29:54 +0200

    dc2

---
+++ Powershell/Opdracht1/DC2/add server to domain.ps1
@@ -0,0 +1,7 @@
+﻿add-computer –domainname "intranet.mijnschool.be"  -restart
+
+Enable-PSRemoting -Force
+
+Enter-PSSession win11-dc2
+
+Install-ADDSDomainController -InstallDns -DomainName "intranet.mijnschool.be"
