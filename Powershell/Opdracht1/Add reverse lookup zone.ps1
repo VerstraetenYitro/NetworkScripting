511bcc7a73f48123e388e91ceaaa0801d9b2c57e
unknown <administrator@INTRANET.MIJNSCHOOL.BE>
2022-09-28 09:10:59 +0200

    add reverse lookup zone

---
+++ Powershell/Opdracht1/Add reverse lookup zone.ps1
@@ -0,0 +1,7 @@
+﻿#add reverse lookup zone
+Add-DnsServerPrimaryZone -NetworkID “192.168.1.0/24” -ReplicationScope "Forest"
+
+Register-DnsClient #ipconfig /registerdns
+
+#disable ipv6
+Disable-NetAdapterBinding -InterfaceAlias "Ethernet0" -ComponentID ms_tcpip6
