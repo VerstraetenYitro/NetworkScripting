aea2488eb83c9615cc602eb2d3922668eb4e7237
unknown <administrator@INTRANET.MIJNSCHOOL.BE>
2022-09-28 09:49:18 +0200

    reverse lookup zone

--- Powershell/Opdracht1/AD Sites and services.ps1
+++ Powershell/Opdracht1/AD Sites and services.ps1
@@ -1,2 +1,17 @@
-﻿#rename default-first-site-name site
-Rename-ADObject -Identity "CN=default-first-site-name,CN=Sites,CN=Configuration,DC=intranet,DC=mijnschool,DC=be" -NewName "Kortijk"
+﻿$newSiteName="Kortrijk"
+$computerName = "win11-DC1"
+
+#rename default-first-site-name site
+
+Rename-ADObject -Identity "CN=default-first-site-name,CN=Sites,CN=Configuration,DC=intranet,DC=mijnschool,DC=be" -NewName $newSiteName
+
+#install dhcp
+$role="DHCP"
+$alreadyInstalled = Get-WindowsFeature -Name $role | % Installed
+if(-not $alreadyInstalled)
+    {
+        Install-WindowsFeature -Name $role -computerName $computerName -IncludeManagementTools -Restart
+    }
+
+#add subnet to site
+New-ADReplicationSubnet -Name "192.168.1.0/24" -Site $newSiteName -Location "Kortrijk,Belgium"
