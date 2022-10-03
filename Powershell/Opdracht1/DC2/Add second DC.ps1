7b1d9b705835239cb76f62b96c26acb92b352a1b
unknown <Administrator@INTRANET.MIJNSCHOOL.BE>
2022-10-03 12:23:14 +0200

    backup

---
+++ Powershell/Opdracht1/DC2/Add second DC.ps1
@@ -0,0 +1,28 @@
+﻿$computerName = "win11-DC2"
+$credential="administrator"
+$domainCredential="$env:INTRANET\$credential"
+$domainname = “intranet.mijnschool.be”
+$netbiosName = “mijnschool”
+$netadapter = Get-NetAdapter -Name "Ethernet0"
+
+Enter-PSSession win11-dc2
+
+#control if roles are already installed, if not install the role.
+
+
+$alreadyInstalled = Get-WindowsFeature -Name "AD-Domain-Services" | % Installed
+if(-not $alreadyInstalled)
+{
+    Install-WindowsFeature -Name "AD-Domain-Services" -computerName $computerName -IncludeManagementTools -Restart
+} 
+
+
+#set DNS correct
+$netadapter | Set-DNSClientServerAddress -ServerAddress ("192.168.1.3","192.168.1.2")
+
+
+#Install-ADDSDomainController -InstallDns -Credential (Get-Credential -Credential $credential) -DomainName $domainname -Confirm
+
+#Install-ADDSDomainController -InstallDns -DomainName $domainname 
+
+Install-ADDSDomainController -InstallDns -Credential (Get-Credential) -DomainName $domainname
--- Powershell/Opdracht1/DC2/add server to domain.ps1
+++ Powershell/Opdracht1/DC2/add server to domain.ps1
@@ -1,7 +1,45 @@
-﻿add-computer –domainname "intranet.mijnschool.be"  -restart
+﻿#
+# Making DC2 server member of the domain
+#
+$credential="administrator"
+$domainCredential="$env:USERDOMAIN\$credential"
+$secondDC="win11-DC2"
+$dnsdomain=$env:USERDNSDOMAIN.tolower()
 
-Enable-PSRemoting -Force
 
-Enter-PSSession win11-dc2
 
-Install-ADDSDomainController -InstallDns -DomainName "intranet.mijnschool.be"
+$remoteSession=New-PSSession -ComputerName $secondDC -Credential $domainCredential
+
+
+
+Invoke-Command -Session $remoteSession -Scriptblock {
+
+
+
+#
+# Install AD Domain Services
+#
+
+
+
+$WindowsFeature="AD-Domain-Services"
+if (Get-WindowsFeature $WindowsFeature -ComputerName $env:COMPUTERNAME | Where-Object { $_.installed -eq $false })
+{
+    write-host "Installing $WindowsFeature ..."
+    Install-WindowsFeature $WindowsFeature -ComputerName $env:COMPUTERNAME -IncludeManagementTools
+}
+else
+{
+    write-host "$WindowsFeature already installed ..."
+}
+
+
+
+#
+# Create Domain Controller
+#
+Install-ADDSDomainController ` -DomainName $args[0] `
+-InstallDns:$true ` -Credential (Get-Credential $args[1]) `
+-Force:$true
+
+} -ArgumentList $dnsdomain, $domainCredential
