9768b2da9f15d980465703731e9735ba56d76b1a
unknown <Administrator@INTRANET.MIJNSCHOOL.BE>
2022-10-03 12:28:06 +0200

    backup

--- Powershell/Opdracht1/Set static IP.ps1
+++ Powershell/Opdracht1/Set static IP.ps1
@@ -3,7 +3,7 @@
 $netadapter | Set-NetIPAddress -DHCP Disabled
 
 $netadapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress 192.168.1.2 -PrefixLength 24 -DefaultGateway 192.168.1.1
-$netadapter | Set-DnsClientDohServerAddress -ServerAddress ("172.20.4.140","172.20.4.141")
+$netadapter | Set-DnsClientServerAddress -ServerAddress ("172.20.4.140","172.20.4.141")
 
 $netadapter | Get-NetIPAddress -AddressFamily IPv4 | ft
 $netadapter | Get-NetRoute
