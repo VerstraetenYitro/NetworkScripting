86853332472f7825e7c9fb2332d8ad021239cf33
unknown <Administrator@INTRANET.MIJNSCHOOL.BE>
2022-10-12 12:02:00 +0200

    backup

--- Powershell/Opdracht1/create_bulk_users.ps1
+++ Powershell/Opdracht1/create_bulk_users.ps1
@@ -1,1 +1,21 @@
-﻿
+﻿$ADUSERS=Import-csv C:\Users\Administrator.WIN11-DC1\Downloads\intranet.mijnschool.be\UserAccounts.csv -Delimiter ';'
+
+foreach($account in $ADUSERS){
+# Create users
+New-ADUser -Name $account.Name `
+-DisplayName $account.DisplayName `
+-Surname $account.Surname `
+-Path $account.Path `
+-SamAccountName $account.SamAccountName `
+-GivenName $account.GivenName `
+-HomeDirectory $account.HomeDirectory`
+-PasswordNeverExpires `
+-ScriptPath $account.ScriptPath`
+-Enabled $true `
+-AccountPassword $account.AccountPassword `
+-ChangePasswordAtLogon $false
+
+
+#Make home dirs
+
+}
