bebb62ccd645130160edb4147ca6810f75599c19
unknown <Administrator@INTRANET.MIJNSCHOOL.BE>
2022-10-12 11:03:50 +0200

    users

--- Powershell/Opdracht1/create_bulk_OU.ps1
+++ Powershell/Opdracht1/create_bulk_OU.ps1
@@ -5,19 +5,23 @@
 Import-Module activedirectory
 
 #Store the data from the CSV in the $ADOU variable. 
-$ADOU = Import-csv C:\it\ou.csv
+$ADOU = Import-csv C:\Users\Administrator.WIN11-DC1\Downloads\intranet.mijnschool.be\OUs.csv -Delimiter ';'
 
 #Loop through each row containing user details in the CSV file
 foreach ($ou in $ADou)
 {
 #Read data from each field in each row and assign the data to a variable as below
+$path = $ou.Path
+$name = $ou.Name
+$Desc=$ou.Description
+$DisplayN=$ou.'Display Name'
 
-$name = $ou.name
-$path = $ou.path
 
 #Account will be created in the OU provided by the $OU variable read from the CSV file
 New-ADOrganizationalUnit `
 -Name $name `
 -path $path `
+-Description $Desc `
+-DisplayName $DisplayN
 
 }
