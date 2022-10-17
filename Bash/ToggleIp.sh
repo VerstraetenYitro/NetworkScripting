28254d198a63d84b773b5d1b2eaf270e36e81ca5
VerstraetenYitro <yitro.verstraeten@student.howest.be>
2022-10-17 12:49:16 +0200

    bash

--- Bash/ToggleIp.sh
+++ Bash/ToggleIp.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
 #
 # Dit script togglet tussen static IP en DHCP IP
 #
+
+ifaceName="ens192"
+ip="192.168.1.124"
+netmask="255.255.255.0"
+gateway="192.168.1.1"
+dns="192.168.1.2 192.168.1.3"
+
+
 
 
