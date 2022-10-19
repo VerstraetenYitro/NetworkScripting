53c43418c36b8d719d069cb9102cc3dd2169f0a2
unknown <Administrator@INTRANET.MIJNSCHOOL.BE>
2022-10-19 10:16:05 +0200

    place users in correct group

--- Powershell/Opdracht1/create_bulk_groups.ps1
+++ Powershell/Opdracht1/create_bulk_groups.ps1
@@ -6,7 +6,6 @@ $groups = Import-Csv C:\Users\Administrator.WIN11-DC1\Downloads\intranet.mijnsch
 foreach ($item in $groups) {
 
     try {
-    Path;Name;DisplayName;Description;GroupCategory;GroupScope
     $path=$item.Path
     $name=$item.Name
     $displayName=$item.DisplayName
@@ -26,5 +25,24 @@ foreach ($item in $groups) {
     }
     catch {
         Write-Host "AD-Object already exists..."
+    }    
+}
+
+#
+# Add users to groups
+#
+$usersAndGroups=Import-Csv C:\Users\Administrator.WIN11-DC1\Downloads\intranet.mijnschool.be\GroupMembers.csv -Delimiter ";"
+
+foreach ($item in $usersAndGroups) {
+
+    try {
+    $groupname=$item.Identity
+    $username=$item.Member
+    Add-ADGroupMember -Identity $groupname -Members $username
+
+    Write-Host "$username is placed in correct group."
+    }
+    catch {
+        Write-Host "User is not placed in correct group."
     }    
 }
