70edd815f13e0154791f8e22e1b85a34115889e6
VerstraetenYitro <yitro.verstraeten@student.howest.be>
2022-11-18 16:33:15 +0100

    added dhcp ranges

--- Bash/dhcp-ranges.sh
+++ Bash/dhcp-ranges.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+sudo apt install isc-dhcp-server -y 
+
+
+
+while IFS="," read -r networkAddress netmask firstIp LastIP
+do
+  echo "subnet $networkAddress netmask $netmask {" >> /etc/dhcp/dhcpd.conf
+  echo "  range $firstIp $LastIP;" >> /etc/dhcp/dhcpd.conf
+  echo "}" >> /etc/dhcp/dhcpd.conf
+  echo "" >> /etc/dhcp/dhcpd.conf
+done < <(tail -n +2 subnets.csv)
+
+
+sudo systemctl restart isc-dhcp-server.service
