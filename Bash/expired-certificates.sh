f391537e955ccc00f4266d0c44159595975796d5
VerstraetenYitro <yitro.verstraeten@student.howest.be>
2022-11-18 17:54:08 +0100

    fixed expired file

--- Bash/expired-certificates.sh
+++ Bash/expired-certificates.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-find / -name '*.pem' -print0 | while IFS= read -d '' -r file; do echo "(" $(date -d "`openssl x509 -in "$file"-text -noout | grep "Not After" | cut -c 25-`" +%s) - $(date -d "now" +%s) ")" / 86400; done
+sudo apt install bc
+
+find / -name '*.pem' -print0 | while IFS= read -d '' -r file
+do 
+command=`openssl x509 -in "$file" -text -noout | grep "Not After" | cut -c 25-`
+days=`echo "(" $(date -d "$command" +%s) - $(date -d "now" +%s) ")" / 86400 | bc`
+INT2=14
+if [ $days -le $INT2 ]; then
+  echo "$file"
+fi
+done
